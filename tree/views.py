47e8567ff6a3e95adfabd99a4ff4c97a4ddfe9a4
Piotr Fryc <piotr.fryc@gmail.com>
2018-11-06 20:08:52 +0100

    working templates

--- tree/views.py
+++ tree/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from .models import Person
+
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello world. You're at tree index")
+	context = { 'person_list': Person.objects.all() }
+	return render(request, 'tree/index.html', context)
+
+def detail(request, person_id):
+	person = get_object_or_404(Person, pk=person_id)
+	return render(request, 'tree/detail.html', { 'person': person })
