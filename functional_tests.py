da0530112051905f9e6bb0ae62afdd2bf045232b
Piotr Fryc <piotr.fryc@gmail.com>
2018-11-12 07:04:36 +0100

    functional tests extended

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class MyTests(unittest.TestCase):
 
-assert 'Django' in browser.title
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_before_login_main_page(self):
+		self.browser.get('http://localhost:8000')
+
+		self.assertIn('Logowanie', self.browser.title)
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
