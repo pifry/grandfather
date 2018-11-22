f6744c64fb428dd21c4e720a39e52119c19eef64
Piotr Fryc <piotr.fryc@gmail.com>
2018-11-22 19:58:00 +0100

    login template

--- grandfather/urls.py
+++ grandfather/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('notifications/', include('django_nyt.urls')),
     path('wiki', include('wiki.urls')),
     path('', include('tree.urls')),
--- tree/urls.py
+++ tree/urls.py
@@ -7,5 +7,5 @@ app_name = 'tree'
 urlpatterns = [
 	path('', views.index, name='index'),
 	#path('wiki/', include('wiki.urls')),
-	#path('<int:person_id>/', views.detail, name='detail'),
+	path('<int:person_id>/', views.detail, name='detail'),
 ]
